d809741f98228eb5812646b5790adde8bb6f7da6
Melissa Kruse <Melissa.Kruse@vu.com>
2022-03-02 23:23:00 -0600

    starter code

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -3,6 +3,10 @@ def calculateTime():
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
+    if (monkey_one == monkey_two):
+        print ("Uh Oh! We're in trouble!")
+    else:
+        print ("Yay! We're going to have a good day!")
 
     # end assignment
 
